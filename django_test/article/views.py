df7cf2e978ed349c0985566f851f4ce36208b086
Mitchell Perez Balajadia <mitchell@Mitchells-MacBook-Air.local>
2014-06-28 16:38:20 -0700

    I added a view in articles and a url in the main project. Simple hello world.

--- django_test/article/views.py
+++ django_test/article/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def hello(request):
+    name = "Mitch"
+    html = "<html><body>Hi %s, this seems to have worked!</body></html>" % name
+    return HttpResponse(html)
