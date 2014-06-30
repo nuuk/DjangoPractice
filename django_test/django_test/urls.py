ae1528a6499eb99b860731efc392a30c85d70d8f
Mitchell Perez Balajadia <mitchell@mitchells-air.corp.ad.wrs.com>
2014-06-29 20:06:40 -0700

    made a bunch of changes. Added a templates directory manually, and called it using a new view

--- django_test/django_test/urls.py
+++ django_test/django_test/urls.py
@@ -10,4 +10,5 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^hello/$', 'article.views.hello'),
+    url(r'^hello_template/$', 'article.views.hello_template'),
 )
